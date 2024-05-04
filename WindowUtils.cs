9f154792c49db800be08d62ba680bb9512c591ff
Andreas Aronsson <BOLL@7708.net>
2024-05-04 10:02:14 +0200

    Convert to class library, update readme, fix WindowsUtils references

--- WindowUtils.cs
+++ WindowUtils.cs
@@ -7,7 +7,9 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Forms;
 
+// ReSharper disable once CheckNamespace
 namespace BOLL7708
 {
     public static class WindowUtils
@@ -18,46 +20,48 @@ namespace BOLL7708
          * - Properties.Resources.Logo.Clone() as System.Drawing.Icon;
          */
         private static System.Windows.Forms.NotifyIcon? _notifyIcon;
-        public static void CreateTrayIcon(Window window, System.Drawing.Icon? icon, string appName) {
+
+        public static void CreateTrayIcon(Window window, System.Drawing.Icon? icon, string appName)
+        {
             if (_notifyIcon != null || icon == null) return;
             _notifyIcon = new()
             {
                 Icon = icon,
                 Text = $"{appName}: Click to show"
             };
-            _notifyIcon.Click += (sender, eventArgs) =>
-            {
-                Restore(window, true);
-            };
+            _notifyIcon.Click += (sender, eventArgs) => { Restore(window, true); };
             _notifyIcon.Visible = true;
         }
+
         /**
          * Run this on OnClosing(), override it on Window.
          */
-        public static void DestroyTrayIcon() {
-            if (_notifyIcon != null)
-            {
-                _notifyIcon.Dispose();
-                _notifyIcon = null;
-            } 
+        public static void DestroyTrayIcon()
+        {
+            if (_notifyIcon == null) return;
+            _notifyIcon.Dispose();
+            _notifyIcon = null;
         }
         // endregion
 
         // region Mutex
         private static Mutex? _mutex = null;
-        public static void CheckIfAlreadyRunning(string appName) {
+
+        public static void CheckIfAlreadyRunning(string appName)
+        {
             _mutex = new Mutex(true, appName, out bool createdNew);
-            if (!createdNew)
-            {
-                MessageBox.Show(
-                Application.Current.MainWindow,
-                "This application is already running!",
-                appName,
-                MessageBoxButton.OK,
-                MessageBoxImage.Information
+            if (createdNew) return;
+            var mainWindow = System.Windows.Application.Current.MainWindow;
+            if(mainWindow != null) {
+                System.Windows.MessageBox.Show(
+                    mainWindow,
+                    "This application is already running!",
+                    appName,
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Information
                 );
-                Application.Current.Shutdown();
             }
+            System.Windows.Application.Current.Shutdown();
         }
         // endregion
 
@@ -89,7 +93,7 @@ namespace BOLL7708
                 case WindowState.Minimized: // For tray icon
                     window.WindowStyle = WindowStyle.ToolWindow;
                     window.ShowInTaskbar = onTaskbar;
-                    break; 
+                    break;
                 default:
                     window.WindowStyle = WindowStyle.ThreeDBorderWindow;
                     window.ShowInTaskbar = true;
