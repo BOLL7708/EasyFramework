680ee3f88ded2b9d31777514f3b445b12b62cb94
Andreas Aronsson <BOLL@7708.net>
2024-05-05 16:56:26 +0200

    Flatten namespace for input dialog

--- SingleInputDialog.xaml.cs
+++ SingleInputDialog.xaml.cs
@@ -1,31 +1,31 @@
 ﻿using System.Windows;
 
-namespace EasyFramework
-{
-    /// <summary>
-    /// Interaction logic for SingleInputDialog.xaml
-    /// </summary>
-    public partial class SingleInputDialog : Window
-    {
-        public string LabelText;
-        public string Value;
-        public SingleInputDialog(Window owner, string value, string labelText)
-        {
-            Owner = owner;
-            this.Value = value;
-            this.LabelText = $"{labelText}:";
-            InitializeComponent();
-            Title = $"Set {labelText}";
-            LabelValue.Content = this.LabelText;
-            TextBoxValue.Text = value;
-            TextBoxValue.Focus();
-            TextBoxValue.SelectAll();
-        }
+namespace EasyFramework;
 
-        private void okButton_Click(object sender, RoutedEventArgs e)
-        {
-            Value = TextBoxValue.Text;
-            DialogResult = Value.Length > 0;
-        }
+/// <summary>
+/// Interaction logic for SingleInputDialog.xaml
+/// </summary>
+public partial class SingleInputDialog : Window
+{
+    public string LabelText;
+    public string Value;
+
+    public SingleInputDialog(Window owner, string value, string labelText)
+    {
+        Owner = owner;
+        this.Value = value;
+        this.LabelText = $"{labelText}:";
+        InitializeComponent();
+        Title = $"Set {labelText}";
+        LabelValue.Content = this.LabelText;
+        TextBoxValue.Text = value;
+        TextBoxValue.Focus();
+        TextBoxValue.SelectAll();
+    }
+
+    private void okButton_Click(object sender, RoutedEventArgs e)
+    {
+        Value = TextBoxValue.Text;
+        DialogResult = Value.Length > 0;
     }
 }
