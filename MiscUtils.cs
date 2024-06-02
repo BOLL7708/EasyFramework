18e40c0c3f53f36b2f350b817a82eb9800363025
Andreas Aronsson <BOLL@7708.net>
2024-06-02 18:15:07 +0200

    Add MiscUtils, first method is to open URLs

---
+++ MiscUtils.cs
@@ -0,0 +1,38 @@
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace EasyFramework;
+
+public static class MiscUtils
+{
+    /**
+     * Based on: https://stackoverflow.com/a/43232486/2076423
+     */
+    public static void OpenUrl(string url)
+    {
+        try
+        {
+            Process.Start(url);
+        }
+        catch
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                url = url.Replace("&", "^&");
+                Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                Process.Start("xdg-open", url);
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                Process.Start("open", url);
+            }
+            else
+            {
+                throw;
+            }
+        }
+    }
+}
